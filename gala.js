8a06a623a006db6163d550bd814ea81252a35806
Aaron Kelley <aaronkelley585@gmail.com>
2019-04-18 11:00:02 -0700

    push

---
+++ gala.js
@@ -0,0 +1,15 @@
+// function pickingNames (){
+
+
+
+// var rand = arry[Math.floor(Math.random() * arry.length)];
+// // console.log(rand)
+// }
+// pickingNames();
+
+var arry= ["Lane","Michael","Mathatse","Brenden","Fabien","Rajiv","PJ","Dwight","Ilker","Tyler","Christopher"];
+
+var rand = arry[Math.floor(Math.random() * arry.length)];
+
+$('body').append(rand);
+
